7c7b1e5c354571c9296510fd06180ce416747597
Smart developer <developer@yandex.ru>
2018-12-24 14:56:46 +0300

    задачи крест и прямоугольник из классной работы

---
+++ cross.py
@@ -0,0 +1,27 @@
+import pygame
+# инициализация Pygame:
+pygame.init()
+# размеры окна:
+size = width, height = [int(x) for x in input().split()]
+# screen — холст, на котором нужно рисовать:
+screen = pygame.display.set_mode(size)
+# формирование кадра:
+
+
+# команды рисования на холсте
+def draw():
+    screen.fill((0, 0, 0))
+    pygame.draw.line(screen, (250, 150, 0), (0, 0), (width, height), 5)
+
+
+
+
+draw()
+
+# смена (отрисовка) кадра:
+pygame.display.flip()
+# ожидание закрытия окна:
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+# завершение работы:
+pygame.quit()
--- rectangle.py
+++ rectangle.py
@@ -10,16 +10,9 @@ screen = pygame.display.set_mode(size)
 
 
 def draw():
-    screen.fill((100, 0, 0))
-    font = pygame.font.Font(None, 50)
-    text = font.render("Hello, Pygame!", 2, (100, 150, 100))
-    text_x = width - text.get_width()
-    text_y = height // 2 - text.get_height() // 2
-    text_w = text.get_width()
-    text_h = text.get_height()
-    screen.blit(text, (100, text_y))
-    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-                                           text_w + 20, text_h + 20), 5)
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, pygame.Color('red'), (1, 1, width - 2, height - 2), 0)
+
 
 
 draw()
