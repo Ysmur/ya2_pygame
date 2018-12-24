e09f1426b534bbe634d4e5b730725fc5cb667125
Smart developer <developer@yandex.ru>
2018-12-24 13:07:34 +0300

    начало работы с Pygame, в репозиторий

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
 import pygame
-
-def draw():
-    screen.fill(100,0,0)
+# инициализация Pygame:
 pygame.init()
+# размеры окна:
 size = width, height = 800, 600
+# screen — холст, на котором нужно рисовать:
 screen = pygame.display.set_mode(size)
-print(1)
+# формирование кадра:
+# команды рисования на холсте
+# ...
+# ...
+# смена (отрисовка) кадра:
 pygame.display.flip()
+# ожидание закрытия окна:
 while pygame.event.wait().type != pygame.QUIT:
     pass
+# завершение работы:
 pygame.quit()
