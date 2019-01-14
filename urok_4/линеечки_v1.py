37d2a4eebcf982969f014e0e60e02a663c1ab0dc
Smart developer <developer@yandex.ru>
2019-01-14 17:26:52 +0300

    линеечки - добавлен метод has_path

--- urok_4/линеечки_v1.py
+++ urok_4/линеечки_v1.py
@@ -56,6 +56,26 @@ class Lines(Board):
         super().__init__(width, height)
         self.selected_cell = None
 
+    def has_path(self, x1, y1, x2, y2):
+        # словарь расстояний
+        d = {(x1, y1): 0}
+        v = [(x1, y1)]
+        while len(v) > 0:
+            x, y = v.pop(0)
+            for dy in range(-1, 2):
+                for dx in range(-1, 2):
+                    if dx * dy != 0:
+                        continue
+                    if x + dx < 0 or x + dx >= self.width or y + dy < 0 or y + dy >= self.height:
+                        continue
+                    if self.board[y + dy][x + dx] == 0:
+                        dn = d.get((x + dx, y + dy), -1)
+                        if dn == -1:
+                            d[(x + dx, y + dy)] = d.get((x, y), -1) + 1
+                            v.append((x + dx, y + dy))
+        dist = d.get((x2, y2), -1)
+        return dist >= 0
+
     def render(self):
         colors = [pygame.Color(0, 0, 255), pygame.Color(255, 0, 0)]
         for i in range(self.width):
