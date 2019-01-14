f73b4008366f4aa115cda7140b7fd3418e67646d
Smart developer <developer@yandex.ru>
2019-01-14 12:41:07 +0300

    жизнь_v1: Выполнен п1.: Расставить клетки на игровом поле с помощью нажатий левой кнопки мышки.

---
+++ urok_4/жизнь_v1.py
@@ -0,0 +1,82 @@
+import pygame
+
+pygame.init()
+size = 470, 470
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height, left=10, top=10, cell_size=15):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 0
+        self.top = 0
+        self.cell_size = 0
+        self.set_view(left, top, cell_size)
+
+    def render(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, pygame.Color(255, 255, 255),
+                                 (x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                                  self.cell_size), 1)
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    # cell - кортеж (x, y)
+    def on_click(self, cell):
+        # заглушка для реальных игровых полей
+        pass
+
+    def get_cell(self, mouse_pos):
+        cell_x = (mouse_pos[0] - self.left) // self.cell_size
+        cell_y = (mouse_pos[1] - self.top) // self.cell_size
+        if cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y >= self.height:
+            return None
+        return cell_x, cell_y
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell)
+
+class Life(Board):
+    def __init__(self, width, height, left=10, top=10, cell_size=30):
+        super().__init__(width, height, left, top, cell_size)
+
+    def on_click(self, cell):
+        self.board[cell[1]][cell[0]] = (self.board[cell[1]][cell[0]] + 1) % 2
+
+    def render(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.board[y][x]:
+                    pygame.draw.rect(screen, pygame.Color('green'),
+                                 (x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                                  self.cell_size))
+                pygame.draw.rect(screen, pygame.Color(255, 255, 255),
+                                 (x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                                  self.cell_size), 1)
+
+
+board = Life(30, 30, 10, 10, 15)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            board.get_click(event.pos)
+    screen.fill((0, 0, 0))
+    board.render()
+    pygame.display.flip()
+    clock.tick(100)
+pygame.quit()
