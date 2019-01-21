f0dff3da60effecd7a67f7de08f105e633282098
small_teather <gym625.ysmur@yandexlyceum.ru>
2019-01-21 17:56:41 +0300

    Boom them all — 2 задача с 6 урока, сейчас лежит в уроке 5

---
+++ urok_5/Boom them all — 2.py
@@ -0,0 +1,71 @@
+import os
+import random
+import pygame
+
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
+
+
+class Bomb(pygame.sprite.Sprite):
+    image = load_image("bomb.png")
+    image_boom = load_image("boom.png")
+
+    def __init__(self, group):
+        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite
+        super().__init__(group)
+        self.image = Bomb.image
+        self.rect = self.image.get_rect()
+        while True:
+            self.rect.topleft = (
+                (random.randint(0, width - self.rect.width), random.randint(0, height - self.rect.height)))
+            if len(pygame.sprite.spritecollide(self, all_sprites, False)) == 1:
+                break
+
+    def update(self):
+        self.rect = self.rect.move(random.randrange(3) - 1, random.randrange(3) - 1)
+
+    def get_event(self, event):
+        if self.rect.collidepoint(event.pos):
+            self.image = self.image_boom
+
+
+clock = pygame.time.Clock()
+
+# группа, содержащая все спрайты
+all_sprites = pygame.sprite.Group()
+
+for i in range(10):
+    # нам уже не нужно даже имя объекта!
+    Bomb(all_sprites)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for bomb in all_sprites:
+                bomb.get_event(event)
+    screen.fill(pygame.Color("white"))
+    all_sprites.draw(screen)
+    all_sprites.update()
+    pygame.display.flip()
+    clock.tick(5)
+
+pygame.quit()
