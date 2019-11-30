ef330ad27e5dfee017d41d1744ab852f817bff43
Elena Muratova <ysmur@yandex.ru>
2019-11-30 23:24:46 +0300

    Урок 2. Файл balls.py - рабочая версия с одним шариком

--- urok_2/balls.py
+++ urok_2/balls.py
@@ -2,11 +2,15 @@ import pygame
 # инициализация Pygame:
 pygame.init()
 # размеры окна:
-size = width, height = 800, 600
+size = width, height = 400, 400
 # screen — холст, на котором нужно рисовать:
 screen = pygame.display.set_mode(size)
 # clock.tick() возвращает количество миллисекунд, прошедших с момента последнего вызова
 clock = pygame.time.Clock()
+
+x = -10
+y = -10
+speed = [0, 0]
 
 # основной цикл
 running = True
@@ -20,11 +24,21 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 screen.fill((0, 0, 0))
-                pygame.draw.circle(screen, (255, 255, 255), event.pos, 20)
+                x, y = event.pos
+                speed = [1, -1]
 
 
     # отрисовка и изменение свойств объектов
+    if x >= width or x <= 0:
+        speed[0] = - speed[0]
+    x += speed[0]
+    if y >= height or y <= 0:
+        speed[1] = - speed[1]
+    y += speed[1]
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, (255, 255, 255), (x, y), 10)
 
 
     # обновление экрана
     pygame.display.flip()
+    clock.tick(100)
